cd78d0a39753222bca760fabaab7d7d08b7c8439
Jamal <jamal@DESKTOP-36B518Q>
2023-12-08 09:24:10 -0600

    phase 8 headache

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,4 +1,27 @@
-// Your code here 
+// Your code here
+class CallCenter{
+	constructor(name){
+		this.name = name
+	}
+
+	sayHello(){
+		console.log(`Hello this is ${this.name}`)
+		//return `Hello this is ${this.name}`
+	}
+
+	callMeLater (delay){
+		//console.log(this.sayHello())
+		setTimeout(() => {
+			this.sayHello()
+		}, delay);
+
+		//return setTimeout => (this.sayHello,delay)
+	}
+}
+
+let judy = new CallCenter("Judy");
+judy.sayHello();         // prints "Hello this is Judy"
+judy.callMeLater(1000);  // waits one second then prints "Hello this is Judy"
 
 
 /*****************************************************************************/
